f5b20ae656026deb00c634ffa0eb18e005f72f96
Amarendra Dash <amarendra.amar.dash@gmail.com>
2024-02-17 00:35:33 +0530

    marquee

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,65 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import "./about.css";
-import { motion } from "framer-motion";
-
-const sliderVariants = {
-  initial: {
-    x: "-120%",
-    y: 0,
-  },
-  animateRight: {
-    x: "550%",
-    transition: {
-      repeat: Infinity,
-      // repeatType: "mirror",
-      duration: 10,
-    },
-  },
-  animateLeft: {
-    x: "-550%",
-    transition: {
-      repeat: Infinity,
-      // repeatType: "mirror",
-      duration: 10,
-    },
-  },
-};
+import Marquee from "react-fast-marquee";
 
 function About() {
-  const [scrollDirection, setScrollDirection] = useState("down");
-  const prevScrollY = useRef(window.pageYOffset);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.pageYOffset;
-      const deltaY = currentScrollY - prevScrollY.current;
-
-      if (deltaY > 0) {
-        setScrollDirection("down");
-      } else {
-        setScrollDirection("up");
-      }
-
-      prevScrollY.current = currentScrollY;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <motion.div className="about-container">
-      <motion.div
-        className="slidingTextContainer"
-        variants={sliderVariants}
-        initial="initial"
-        animate={scrollDirection === "down" ? "animateRight" : "animateLeft"} // Slide right when scrolling down, left when scrolling up
-      >
-        <p>Writer content creator</p>
-      </motion.div>
-    </motion.div>
+    <div className="about-container">
+      <div className="slidingTextContainer">
+        <Marquee speed={200}>&nbsp;Amarendra Dash -</Marquee>
+      </div>
+    </div>
   );
 }
 
